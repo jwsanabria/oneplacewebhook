b6c5ec0fae27d6134cde9422d415cc94f95aacc0
Fabio Bolívar <fe.bolivar@uniandes.edu.co>
2020-10-18 22:32:59 -0500

    Creación  modelo BD para interacciones con Whatsapp

---
+++ src/models/BDWhatsapp.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const WhastappMsgSchema = new Schema({
+    MessageSid: {type: String, required: true},
+    To: {type: String, required: true},
+    From: {type: String, required: true},
+    Body: {type: String, required: true},
+    Owner: {type: Number, required: true}, //1-From, 2-To.
+    Hour: {type: Date, default: Date.now}
+});
+
+const WhatsappUserAccountSchema = new Schema({
+    UserId: {type: String, required: true},
+    AccountType: {type: Number, required: true}, //1-WS, 2-FB
+    AccountId: {type: String, required: true}
+});
+
+module.exports = mongoose.model('WhastappMsg', WhatsappUserAccountSchema)
+module.exports = mongoose.model('WhatsappUserAccount', WhatsappUserAccountSchema)
---
+++ src/whatsapp/wsroutine.js
@@ -0,0 +1,19 @@
+//Se busca en la tabla de cuentas por usuario qué cuentas le pertenecen
+function getWSUserAccounts(UserId)
+{
+
+
+}
+
+//Se filtra por los telefonos encontrados de esa persona. Se muestra  el último mensaje por cada persona
+function getWSContactMSG_ByUser(WSUserAccountId)
+{
+
+
+}
+
+//Se consulta la BD con esos dos parámetros, se ordena por fecha desc
+function getWSMessagesByFromTo(From, To)
+{
+
+}
