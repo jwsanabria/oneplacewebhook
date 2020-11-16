bbcc0a7924977db9bdef9f4ffc455c96a548fb96
Fabio Bolívar <fe.bolivar@uniandes.edu.co>
2020-11-15 19:46:45 -0500

    Ajuste CORS
    Ajuste CORS

--- src/server.js
+++ src/server.js
@@ -20,11 +20,11 @@ app.set('view engine', 'hbs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', req.get('Origin') || '*');
+    //res.header('Access-Control-Allow-Origin', req.get('Origin') || '*');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
     res.header('Access-Control-Expose-Headers', 'Content-Length');
-    res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
+    //res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
     //20201115 fe.bolivar. Se agregan los headers para prevenir CORS
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
